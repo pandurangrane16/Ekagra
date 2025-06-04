474da4118597d039a0aa6c2c312dbcaf8359c1f8
Pandurang Rane <pandurangrane16@gmail.com>
2025-06-04 14:47:50 +0530

    Material Common Control 1.2

---
+++ src/app/common/cm-table/cm-table.component.ts
@@ -0,0 +1,91 @@
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
+import { MaterialModule } from '../../Material.module';
+import { CommonModule } from '@angular/common';
+import { CmPaginationComponent } from '../cm-pagination/cm-pagination.component';
+import { MatMenuPanel } from '@angular/material/menu';
+import { Router } from 'express';
+
+@Component({
+  selector: 'app-cm-table',
+  imports: [MaterialModule,CommonModule,CmPaginationComponent],
+  templateUrl: './cm-table.component.html',
+  styleUrl: './cm-table.component.css'
+})
+export class CmTableComponent implements OnInit {
+  listOfData: any;
+    tooltip: string = "";
+    searchText: string = "";
+    @Input() _headerName:string = "";
+    @Input() pagination:boolean =true;
+    @Input() isSearch: boolean = false;
+    @Output() pager = new EventEmitter<number>();
+    @Output() searchWithId = new EventEmitter<any>();
+    @Output() search = new EventEmitter<string>();
+    @Output() recordPerPage = new EventEmitter<number>();
+    @Input() headArr: any[] = [];
+    @Input() link!: string;
+    @Input() isSearchBox: boolean = true;
+    @Input() fieldName!: string;
+    @Input() gridArr: any[] = [];
+    @Input() totalRecords!: number;
+    @Input() perPage: number = 10;
+    @Input() totalPages: number = 1;
+    @Input() collectionSize: number = 1;
+    @Input() btnArray: any[] = [];
+    filteredData: any = [];
+    activePage: number = 0;
+    @Output() btnAction = new EventEmitter<any>();
+    @Output() checked = new EventEmitter<any>();
+    @Output() notChecked = new EventEmitter<any>();
+  menu1: MatMenuPanel<any>|null;
+    constructor(private router: Router) {
+  
+    }
+    ngOnInit(): void {
+      this.headArr = this.headArr.sort(x=>x.position);
+    }
+    ngOnChanges(changes: SimpleChanges): void {
+      if(!this.isSearch) {
+        this.searchText = "";
+      }
+    }
+    displayActivePage(activePageNumber: number) {
+      this.activePage = activePageNumber
+    }
+    Search() {
+      if (this.searchText.trim().length > 2) {
+        this.search.emit(this.searchText);
+      } else if (this.searchText.trim() == "") {
+        this.search.emit(this.searchText);
+      }
+    }
+    mouseEnter(msg: string) {
+      this.tooltip = msg;
+    }
+    pageChange(pager: number) {
+      this.pager.emit(pager);
+    }
+  
+    onPageChange(pageNo: number) {
+      this.pageChange(pageNo);
+    }
+    onPageRecordsChange(pageNo: number) {
+      this.recordPerPage.emit(pageNo);
+    }
+  
+    ShowForm(item: any) {
+      if (this.btnArray.length == 0)
+        this.searchWithId.emit(item);
+      //this.router.navigate([this.link]);
+    }
+    GoToBtnAction(evt:any, data: any) {
+      let _sendData = {"event":evt, "data": data };
+      this.btnAction.emit(_sendData);
+    }
+    Checked(eve: any, data: any) {
+      if (eve.target.checked == true)
+        this.checked.emit(data);
+      else
+        this.notChecked.emit(data);
+    }
+  }
--- src/app/common/cm-toggle/cm-toggle.component.ts
+++ src/app/common/cm-toggle/cm-toggle.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MaterialModule } from '../../Material.module';
 
 @Component({
   selector: 'app-cm-toggle',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './cm-toggle.component.html',
   styleUrl: './cm-toggle.component.css'
 })
 export class CmToggleComponent {
-
+  @Input() settings:any;
 }
