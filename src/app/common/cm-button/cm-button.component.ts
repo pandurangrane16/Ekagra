4d0e82089bbc9a4c0713edf518ed8d5609088024
Pandurang Rane <pandurangrane16@gmail.com>
2025-06-04 14:25:49 +0530

    common input and button

--- src/app/common/cm-button/cm-button.component.ts
+++ src/app/common/cm-button/cm-button.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MaterialModule } from '../../Material.module';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cm-button',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule,CommonModule],
   templateUrl: './cm-button.component.html',
   styleUrl: './cm-button.component.css'
 })
 export class CmButtonComponent {
+  @Input() color: any;
+  @Input() class: any;
+  @Input() type: any;
 
+  @Output() buttonClicked = new EventEmitter<string>();
+
+  onButtonClicked() {
+    this.buttonClicked.emit('This Is Clicked');
+  }
 }
--- src/app/common/cm-input/cm-input.component.ts
+++ src/app/common/cm-input/cm-input.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Input } from '@angular/core';
+import { MaterialModule } from '../../Material.module';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cm-input',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule,
+    MatInputModule,MaterialModule,CommonModule],
   templateUrl: './cm-input.component.html',
   styleUrl: './cm-input.component.css'
 })
 export class CmInputComponent {
+  @Input() _inputData:any;
+  value: string = '';
+  type : string= "text";
+  @Input() formGroup : any;
+  @Input() controlName: any; 
+  constructor(private cdRef: ChangeDetectorRef){
+    
+  }
+  ngOnInit(): void {
+    if(this._inputData.type != undefined)
+      this.type = this._inputData.type;
+  }
+ // Function to call when the value changes
+ onChange: any = () => {};
 
+ // Function to call when the input is touched
+ onTouched: any = () => {};
+
+ // Write value from the form model into the view
+ writeValue(value: string): void {
+   this.value = value || '';
+ }
+
+ // Register a callback function to notify Angular when the value changes
+ registerOnChange(fn: any): void {
+   this.onChange = fn;
+ }
+
+ // Register a callback function to notify Angular when the input is touched
+ registerOnTouched(fn: any): void {
+   this.onTouched = fn;
+ }
+
+ // Handle input changes
+ onInput(event: Event): void {
+   const value = (event.target as HTMLInputElement).value;
+   this.value = value;
+   this.onChange(value); // Notify Angular of the change
+ }
+
+ // Handle blur event
+ onBlur(): void {
+   this.onTouched(); // Notify Angular that the input was touched
+ }
+
+ toggleDisable() {
+  this._inputData.isDisabled = !this._inputData.isDisabled;
+  this.cdRef.detectChanges();  // Manually trigger change detection if necessary
+}
 }
