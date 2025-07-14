0a4b803b2625b9ceacb45eec569edc9b58374cc1
Pandurang Rane <pandurangrane16@gmail.com>
2025-07-14 14:33:16 +0530

    select2 changes

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { FooterComponent } from '../footer/footer.component';
@@ -6,21 +6,34 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { RouterModule } from '@angular/router';
-import { SessionService } from '../../services/common/session.service';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { loginservice } from '../../services/admin/login.service';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, SlickCarouselModule, FooterComponent, MatFormFieldModule, MatInputModule, MatIconModule, RouterModule],
+  imports: [CommonModule, SlickCarouselModule, MatButtonModule, ReactiveFormsModule, FooterComponent, MatFormFieldModule, MatInputModule, MatIconModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent {
-  version: string = "2.0.0.0";
-  sessionService = inject(SessionService);
-  constructor() {
 
-  }
+  loginForm!: FormGroup;
+  version: string = "2.0.0.0";
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+    private service: loginservice
+  ) { }
+
+
+
   slideConfig = {
     infinite: true,
     slidesToShow: 1,
@@ -60,7 +73,7 @@ export class LoginComponent {
     { img: '/assets/img/login_bg4.jpg' },
   ];
 
-    ngOnInit(): void {
+  ngOnInit(): void {
     this.loginForm = this.fb.group({
       userNameOrEmailAddress: ['', Validators.required],
       password: ['', Validators.required]
@@ -68,18 +81,18 @@ export class LoginComponent {
   }
   showPassword: boolean = false;
 
-togglePasswordVisibility(): void {
-  this.showPassword = !this.showPassword;
-}
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
 
-   onLogin(): void {
+  onLogin(): void {
     const credentials = this.loginForm.value;
 
     this.service.Login(credentials).subscribe({
       next: (res) => {
         const accessToken = res?.result?.accessToken;
         if (accessToken) {
-      
+
           this.router.navigate(['/dashboard']);
         } else {
           alert('Login failed: No token received');
@@ -88,7 +101,7 @@ togglePasswordVisibility(): void {
       error: (err) => {
         console.error('Login error:', err);
         alert('Login failed: Invalid credentials or server error');
-          this.loginForm.reset(); 
+        this.loginForm.reset();
       }
     });
   }
