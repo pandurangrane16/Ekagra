f2fffbc915bc1d388d8224fab3667ac552db0534
anup_jadhav <anup_jadhav@cms.co.in>
2025-07-30 17:02:28 +0530

    Surveilience - Live feed -camera stream

--- src/app/routes/surveilience-camera/surveilience-camera.component.spec.ts
+++ src/app/routes/surveilience-camera/surveilience-camera.component.spec.ts
@@ -1,14 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SurveilienceCameraComponent } from './surveilience-camera.component';
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 describe('SurveilienceCameraComponent', () => {
   let component: SurveilienceCameraComponent;
   let fixture: ComponentFixture<SurveilienceCameraComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SurveilienceCameraComponent]
+      imports: [SurveilienceCameraComponent],
+       schemas: [CUSTOM_ELEMENTS_SCHEMA] 
     })
     .compileComponents();
     
