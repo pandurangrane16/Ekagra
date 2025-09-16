13822504e9cd95142b24f6d5926d68c618d09706
Harshita <harshitajaswani222@gmail.com>
2025-09-16 16:44:18 +0530

    ..

--- src/app/admin/api-playground/api-playground.component.ts
+++ src/app/admin/api-playground/api-playground.component.ts
@@ -412,7 +412,7 @@ isSelected(key: string): boolean {
         qsNo: index++,
         qsKey: key,
         qsValue: '',
-        qsType: { name: 'URL', value: '6' },       // or change type if needed
+        qsType: { name: 'AuthenticationInput', value: '3' },       // or change type if needed
         qsInputType: { name: 'Static', value: '3' },
         qsSelectedType: ''
       });
