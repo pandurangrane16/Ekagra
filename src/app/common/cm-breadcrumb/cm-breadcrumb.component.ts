b32dd18e7425b8654a86290c35006fd103804d58
swapnali birmole <swapnalibirmole@gmail.com>
2025-12-30 14:32:52 +0530

    Added menu icons for PA, TES, VMS and page heading for PA

--- src/app/common/cm-breadcrumb/cm-breadcrumb.component.ts
+++ src/app/common/cm-breadcrumb/cm-breadcrumb.component.ts
@@ -1,6 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { CmSelectCheckComponent } from '../cm-select-check/cm-select-check.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
 
 export interface BreadcrumbRow {
   index?: number;
@@ -11,13 +20,14 @@ export interface BreadcrumbRow {
 @Component({
   selector: 'app-cm-breadcrumb',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,MatButtonToggleModule,MatDatepickerModule,CmSelectCheckComponent,  MatIconModule, MatFormFieldModule, MatSelectModule, MatInputModule, MatButtonModule,MatTooltipModule],
   templateUrl: './cm-breadcrumb.component.html',
   styleUrls: ['./cm-breadcrumb.component.css']
 })
 export class CmBreadcrumbComponent {
   @Input() rows: BreadcrumbRow[] = [];
-
+startDate:any;
+endDate:any;
   @Input('breadcrumbItems')
   set breadcrumbItems(items: BreadcrumbRow[] | undefined) {
     if (items) {
@@ -31,4 +41,31 @@ export class CmBreadcrumbComponent {
       this.rows = items;
     }
   }
+
+  DateWiseFilter(evtData: any, category: string) {
+  const localDate = new Date(evtData.value);
+
+  // Convert date to UTC explicitly
+  const utcDate = new Date(
+    localDate.getUTCFullYear(),
+    localDate.getUTCMonth(),
+    localDate.getUTCDate(),
+    localDate.getUTCHours(),
+    localDate.getUTCMinutes(),
+    localDate.getUTCSeconds()
+  );
+
+  // Convert to Unix UTC timestamp in milliseconds (13 digits)
+  const unixUtcMs = utcDate.getTime();  // 💥 No /1000 here
+
+  if (category === "start") {
+    this.startDate = unixUtcMs;
+    console.log("Start Date UTC UNIX (ms):", unixUtcMs);
+  } else {
+    this.endDate = unixUtcMs;
+    console.log("End Date UTC UNIX (ms):", unixUtcMs);
+  }
+
+  //this.GetOngoingAnnoucement();
+}
 }
